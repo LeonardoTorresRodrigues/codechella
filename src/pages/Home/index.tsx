7acb2db1cfcd50a25eee4c04a5ead5df8e16a500
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-01 20:10:38 -0300

    feat: new home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,10 @@
 import Header from '../../components/Header';
 import style from './Home.module.scss';
 
-function App() {
+export default function Home() {
   return (
     <div className={style.menu}>
       <Header />
     </div>
   );
 }
-
-export default App;
