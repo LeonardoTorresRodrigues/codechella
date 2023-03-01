616873c2c8cc800845bbc46c5347eed81b477459
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-01 17:37:24 -0300

    refactor: import new style and component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import 'normalize.css';
-import App from './App';
+import './index.css';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
