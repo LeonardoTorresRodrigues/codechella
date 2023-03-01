a5ef2f714ad7e06cbc60fb470e2f23175b361be3
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-01 20:11:57 -0300

    feat: new header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
 import style from './Header.module.scss';
-import { ReactComponent as Logo } from 'assets/Logo.svg';
+import { ReactComponent as Logo } from 'assets/white_logo.svg';
 
 export default function Header() {
   return (
-    <div className={style.nav}>
+    <div className={style.header}>
       <Logo />
+      <nav className={style.header__nav}>
+        <a className={style.header__link} href="">A experiência</a>
+        <a className={style.header__link} href="">Mapa de Setores</a>
+        <a className={style.header__link} href="">Informações</a>
+        <a className={style.header__link} href="">Ingresso</a>
+      </nav>
     </div>
   );
 }
