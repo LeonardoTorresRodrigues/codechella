ca513e9ad283323234a5cb58f4cb5a2f669747c8
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-01 17:30:41 -0300

    feat: create page folder with Home page
    n

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header';
+import style from './Home.module.scss';
+
+function App() {
+  return (
+    <div className={style.menu}>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
