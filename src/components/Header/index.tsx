d439718fedabdd6a27762c2608ae975187c592de
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-20 13:14:15 -0300

    feat: add hamburger-react nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,43 @@
 import style from './Header.module.scss';
-import { ReactComponent as Logo } from 'assets/white_logo.svg';
-import Banner from 'components/Banner';
+import logo from 'assets/white_logo.png';
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
 
 export default function Header() {
+
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div>
       <div className={style.header}>
-        <Logo />
-        <nav className={style.header__nav}>
-          <a className={style.header__link} href="#">A experiência</a>
-          <a className={style.header__link} href="#">Mapa de Setores</a>
-          <a className={style.header__link} href="#">Informações</a>
-          <a className={style.header__link} href="#">Ingresso</a>
+        <img src={logo} alt="<CodeChella/>" />
+        <Hamburger
+          color='FFF'
+          easing='ease-in'
+          label='Abrir menu de navegação'
+          size={32}
+          toggled={isOpen}
+          toggle={setOpen}
+        />
+        {isOpen && (
+          <nav className={style.header_nav}>
+            <ul className={style.header_ul}>
+              <li className={style.header_li} ><a className={style.header_a} href="#">A experiência</a></li >
+              <li className={style.header_li}><a className={style.header_a} href="#">Mapa de Setores</a></li>
+              <li className={style.header_li}><a className={style.header_a} href="#">Informações</a></li>
+              <li className={style.header_li}><a className={style.header_a} href="#">Ingresso</a></li>
+            </ul>
+          </nav>
+        )}
+        <nav className={style.nav}>
+          <ul>
+            <li><a href="#">A experiência</a></li>
+            <li><a href="#">Mapa de Setores</a></li>
+            <li><a href="#">Informações</a></li>
+            <li><a href="#">Ingresso</a></li>
+          </ul>
         </nav>
       </div>
-      <Banner />
     </div>
   );
 }
